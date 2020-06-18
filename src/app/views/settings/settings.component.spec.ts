042ae45e52608f3f8e1f6c00b9f853f2c7b3949f
Fabien THOMAS <fab_thomas@msn.com>
2020-06-18 19:05:27 +0200

    Api key (#10)
    * adding settings page to set api key
    
    * changelog

---
+++ src/app/views/settings/settings.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SettingsComponent } from './settings.component';
+import { ApiKeyService } from 'src/app/services/api-key.service';
+
+describe('SettingsComponent', () => {
+  let component: SettingsComponent;
+  let fixture: ComponentFixture<SettingsComponent>;
+
+  beforeEach(async(() => {
+    const spy = jasmine.createSpyObj('ApiKeyService', ['getKey',' setKey']);
+    spy.getKey.and.returnValue('abcef');
+    TestBed.configureTestingModule({
+      declarations: [ SettingsComponent ],
+      providers: [
+        {provide: ApiKeyService, useValue: spy}
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SettingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/views/settings/settings.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ApiKeyService } from 'src/app/services/api-key.service';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent implements OnInit, OnDestroy {
+
+  public keyControl: FormControl = new FormControl();
+  private keyControlSub: Subscription;
+
+  constructor(private apiKeyService: ApiKeyService) {
+  }
+
+  ngOnInit(): void {
+    this.keyControl.setValue(this.apiKeyService.getKey());
+
+    this.keyControlSub = this.keyControl.valueChanges.subscribe({
+      next: (value) => {
+        this.apiKeyService.setKey(value);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.keyControlSub.unsubscribe();
+  }
+}
